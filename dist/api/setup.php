54f898ddbff15a903bdb7bd7935434b6a58bfc21
Kiyoshi Hino <kiyoshihino@users.noreply.github.com>
2026-03-19 17:30:37 -0300

    build: atualiza dist com novo conteúdo e suporte a Bio

--- dist/api/setup.php
+++ dist/api/setup.php
@@ -64,12 +64,12 @@ try {
             './assets/logo.png', 
             '+55 61 99259-0209', 
             '5561992590209', 
-            'Qri 15 casa 12c 5 - Santa Maria (Residencial Santos Dumont), Brasília - DF', 
-            'Santa Maria, Brasília e região', 
+            'QBR8, bloco O, Residencial Santos Dumont, DF.', 
+            'Residencial Santos Dumont, DF', 
             'bialobodoces', 
             'bialobodoces', 
             'contato@bialobodoces.com.br', 
-            'Há mais de 8 anos, Bia Lobo transforma ingredientes simples em experiências doces únicas. Nascida em Brasília e apaixonada pela arte da confeitaria, ela descobriu seu dom de criar doces artesanais que contam histórias e conectam pessoas.', 
+            'A Bia Lobo Doces nasceu de forma simples e despretensiosa, entre receitas feitas para a família e para amigos. O que começou como um gesto de carinho logo se transformou em uma grande paixão pela confeitaria.\nMovida pelo desejo constante de aprender e se aperfeiçoar, Bia mergulhou no estudo das técnicas da confeitaria, explorando sabores, massas e texturas. Cada receita passou a ser desenvolvida com cuidado, pesquisa e dedicação, sempre buscando entregar algo especial e memorável.\n\nAssim surgiu a Bia Lobo Doces: uma confeitaria que valoriza o sabor, a qualidade dos ingredientes e a experiência de cada cliente. Cada doce é pensado com atenção aos detalhes, preparado com carinho e feito para participar de momentos importantes e celebrar histórias únicas.\n\nPor trás de cada criação existe muito mais do que técnica: existe amor pelo que se faz. Bia é mãe, esposa, professora e uma apaixonada pela arte de transformar ingredientes em doces que encantam.', 
             '8+'
         )");
     }
